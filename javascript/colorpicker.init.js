c76ba96ad4811e64af4a6297b18ef3e85cac216c
Tom Densham <tomdensham@googlemail.com>
2016-03-16 12:03:58 +0000

    Update field text color on change
    Just added the same black/white text color change to the JS change event.

--- javascript/colorpicker.init.js
+++ javascript/colorpicker.init.js
@@ -12,8 +12,16 @@
         $('.color-picker').iris({
           palettes: palettes,
           change: function (event, ui) {
-            var $c, $r, $g, $b, $mid;
-            $(this).css('backgroundColor', ui.color.toString());
+            var value = ui.color.toRgb(),
+                textColor = '#000000';
+
+            if ((value.r + value.g + value.b) < 128) {
+              textColor = '#ffffff';
+            }
+
+            $(this)
+                .css('color', textColor)
+                .css('backgroundColor', ui.color.toString());
           }
         });
         this.on('click', function (e) {
